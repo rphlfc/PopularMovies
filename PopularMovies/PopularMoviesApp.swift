f7187beb5b841ca10cbd10073bfc974e44c458a7
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-29 05:20:09 -0300

    Initial Commit

---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Raphael Cerqueira on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
